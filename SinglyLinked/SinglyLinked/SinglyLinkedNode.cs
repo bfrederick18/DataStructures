17f094502f54cc730939b8f8b02105d2c833db95
Brandon Frederick <1ultimatedj@gmail.com>
2017-06-03 16:21:54 -0700

    Initial Commit

---
+++ SinglyLinked/SinglyLinked/SinglyLinkedNode.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinked
+{
+    class SinglyLinkedNode<T>
+    {
+        #region GetSet
+        public T Thing
+        {
+            get;
+            set;
+        }
+        public SinglyLinkedNode<T> Next
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        private SinglyLinkedNode(T thingToStore, SinglyLinkedNode<T> next = null)
+        {
+            Thing = thingToStore;
+            Next = next;
+        }
+        public SinglyLinkedNode<T> AddAfter(T thingToStore)
+        {
+            SinglyLinkedNode<T> node = new SinglyLinkedNode<T>(thingToStore);
+
+            Next = node;
+
+            return node;
+        }
+
+        public bool CanDeleteAfter()
+        {
+            SinglyLinkedNode<T> node = Next;
+            if (node.Next.Next != null)
+            {
+                return false;
+            }
+            else return true;
+        }
+
+        public SinglyLinkedNode<T> DeleteAfter()
+        {
+
+        }
+    }
+}
