a5a460a51218183548a06f9889f3d8897b9f5b59
Brandon Frederick <1ultimatedj@gmail.com>
2017-06-10 12:21:52 -0700

    Added Insert

--- SinglyLinked/SinglyLinked/SinglyLinkedList.cs
+++ SinglyLinked/SinglyLinked/SinglyLinkedList.cs
@@ -9,6 +9,7 @@ namespace SinglyLinked
     public class SinglyLinkedList
     {
         SinglyLinkedNode<int> head;
+        int count = 0;
         public SinglyLinkedList()
         {
             head = null; 
@@ -19,6 +20,7 @@ namespace SinglyLinked
             if (head == null)
             {
                 head.AddAfter(thingToStore);
+                count++;
             }
 
             else
@@ -29,12 +31,42 @@ namespace SinglyLinked
                     if (tempNode.Next == null)
                     {
                         tempNode.AddAfter(thingToStore);
+                        count++;
                     }
                     else
                     {
                         tempNode = tempNode.Next;
                     }
                 }
+            }
+        }
+
+        public void Insert(int thingToStore, int index)
+        {
+            if (index > count)
+            {
+                Console.WriteLine("Plez... RETINK UR DISKISION\nERROR ERROR ERROR");
+            }
+
+            if (index == 0)
+            //doblee check with teeessstts
+            {
+                SinglyLinkedNode<int> tempNode = new SinglyLinkedNode<int>(thingToStore);
+
+                tempNode.Next = head;
+
+                head = tempNode;
+            }
+
+            else
+            {
+                SinglyLinkedNode<int> tempNode = head;
+                for (int i = 0; i < index; i++)
+                {
+                    tempNode = tempNode.Next;
+                }
+
+                tempNode.AddAfter(thingToStore);
             }
         }
     }
