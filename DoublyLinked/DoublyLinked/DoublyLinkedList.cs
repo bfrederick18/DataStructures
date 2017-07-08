dc409bf84d116ee679c9d7ec53f4b65f72e72dd2
Brandon Frederick <1ultimatedj@gmail.com>
2017-07-08 12:15:13 -0700

    Added AddToEnd and AddToFront

--- DoublyLinked/DoublyLinked/DoublyLinkedList.cs
+++ DoublyLinked/DoublyLinked/DoublyLinkedList.cs
@@ -8,6 +8,53 @@ namespace DoublyLinked
 {
     public class DoublyLinkedList
     {
-        
+        DoublyLinkedNode<int> head;
+        int count = 0;
+        public DoublyLinkedList()
+        {
+            head = null;
+        }
+
+        public void AddToEnd(int thingToStore)
+        {
+            if (head == null)
+            {
+                head = new DoublyLinkedNode<int>(thingToStore);
+                count++;
+            }
+
+            else
+            {
+                DoublyLinkedNode<int> tempNode = head;
+                while (true)
+                {
+                    if (tempNode.Next == null)
+                    {
+                        tempNode.AddAfter(thingToStore);
+                        count++;
+                        break;
+                    }
+
+                    else
+                    {
+                        tempNode = tempNode.Next;
+                    }
+                }
+            }
+        }
+        public void AddToFront(int thingToStore)
+        {
+            if (head == null)
+            {
+                head = new DoublyLinkedNode<int>(thingToStore);
+            }
+
+            else
+            {
+                DoublyLinkedNode<int> node = new DoublyLinkedNode<int>(thingToStore, null, head);
+                head = node;
+                count++;
+            }
+        }
     }
 }
