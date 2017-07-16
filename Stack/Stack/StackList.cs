13355d697f318a6f7a1cc7f0fc63f64e4a7d51ce
Brandon Frederick <1ultimatedj@gmail.com>
2017-07-16 11:30:48 -0700

    Cleared StackList

--- Stack/Stack/StackList.cs
+++ Stack/Stack/StackList.cs
@@ -15,48 +15,7 @@ namespace Stack
             head = null;
         }
 
-        public void AddAfter(int thingToStore)
-        {
-            if (head == null)
-            {
-                head = new StackNode<int>(thingToStore);
-                count++;
-            }
-
-            else
-            {
-                StackNode<int> tempNode = head;
-                while (true)
-                {
-                    if (tempNode.Next == null)
-                    {
-                        tempNode.AddAfter(thingToStore);
-                        count++;
-                        break;
-                    }
-                    else
-                    {
-                        tempNode = tempNode.Next;
-                    }
-                }
-            }
-        }
-        public void AddBefore(int thingToStore)
-        {
-            if (head == null)
-            {
-                head = new StackNode<int>(thingToStore);
-                count++;
-            }
-
-            StackNode<int> tempNode = new StackNode<int>(thingToStore);
-
-            tempNode.Next = head;
-
-            head = tempNode;
-            count++;
-        }
-
+        
 
     }
 }
